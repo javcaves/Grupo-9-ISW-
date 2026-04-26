4abff46997fc3b2bf9c5a3d1c1bf10cefe13723a
Javiera Cuevas <104380487+javcaves@users.noreply.github.com>
2026-04-26 17:40:05 -0400

    Update items.controller.js

---
+++ backend/src/modules/bodega/items/items.controller.js
@@ -0,0 +1,30 @@
+/**
+ *@file items.controller.js
+ *@description Controlar para items de bodega
+ */
+//1. IMPORTS
+import * as ItemsService from './items.service.js';
+
+//2. HELPER FUNCTIONS
+
+
+//3. MAIN FUNCTIONS 
+
+//################# CREAR #################
+
+
+//################# LEER #################
+
+
+//################# ACTUALIZAR  #################
+
+
+//################# ELIMINAR  #################
+
+
+
+
+//4. EXPORTS
+module.exports = {
+  
+}
