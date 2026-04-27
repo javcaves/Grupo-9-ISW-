5d1de6e84a4142dbb94020f99227005f6e008f5e
am <antonia.pena2202@alumnos.ubiobio.cl>
2026-04-26 22:40:03 -0400

    update categorias.controller y termino de categorias.routes
    me faltó una función por validar en el controller para poder hacer el routes pero ya están listos (creo)

---
+++ backend/src/modules/gestion_actividades/categorias/categorias.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import * as CategoriasCtrl from './categorias.controller.js';
+
+const router = Router();
+
+//rutas de lectura******
+router.get('/', CategoriasCtrl.listarTodas); //obtener todas las categorias 
+router.get('/activas', CategoriasCtrl.listarActivas); //obtener solo categorias activas
+router.get('/buscar', CategoriasCtrl.buscarCatC); //buscar categoria por nombre o descripcion
+
+//rutas de escritura*****
+router.get('/crear', CategoriasCtrl.crearCategoriaC); //crear categoria nueva
+router.get('/actualizar', CategoriasCtrl.actualizarCatC); //editar categoria existente
+
+//rutas de eliminacion (solo eliminacion soft)*****
+router.get('/:id', CategoriasCtrl.desactivarCatC); //desactivar categoria
