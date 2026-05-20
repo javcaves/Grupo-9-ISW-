cd60173c6191c533e469a4acdd5fafd5f8c51af5
Javiera Cuevas <104380487+javcaves@users.noreply.github.com>
2026-05-20 01:00:25 -0400

    Update item.entity.js

--- backend/src/entity/item.entity.js
+++ backend/src/entity/item.entity.js
@@ -21,7 +21,7 @@ export class Item {
         enum: [
             "MAQUINARIA",
             "HERRAMIENTA",
-            "UTENCILIO",
+            "UTENSILIO",
             "PRODUCTO"
         ]
     })
