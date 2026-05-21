20a0a7a5f71d8b658738bca8b2a2c68349f825f9
ElfoAlienigena <amita.guzmancid@gmail.com>
2026-05-20 20:39:53 -0400

    instalado node-cron

--- backend/src/modules/asistencia/asistencia.cron.js
+++ backend/src/modules/asistencia/asistencia.cron.js
@@ -1,3 +1,7 @@
+// The node-cron module is tiny task scheduler in pure JavaScript for node.js 
+// based on GNU crontab. This module allows you to schedule task in node.js using full crontab syntax.
+// https://www.npmjs.com/package/node-cron
+
 import cron from "node-cron";
 import { ejecutarCierreAutomaticoAsistencia } from "./module/asistencia/asistencia.service.js";
 
